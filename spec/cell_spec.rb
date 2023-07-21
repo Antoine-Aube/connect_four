8451a326f07aa17aac57bed9a68375b4dc21c14a
Antoine Aube <aaube3@gmail.com>
2023-07-21 15:17:47 -0600

    feat: cell has starting state with corresponding test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,8 +4,16 @@ RSpec.describe Cell do
   describe "#initialize" do 
     it "it can exist" do 
       cell = Cell.new
-
+      
       expect(cell).to be_a Cell
     end
+  end
+  
+  describe "#set_state" do 
+    it "has a starting state of a dot(period)" do 
+      cell = Cell.new
+      
+      expect(cell.state).to eq(".")
+    end   
   end
 end
