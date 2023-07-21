a5685591590dfd768b68cfc09ff321ba727a26f7
Antoine Aube <aaube3@gmail.com>
2023-07-21 15:40:59 -0600

    feat; player has a piece instance variable

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,7 @@
 class Player 
+  attr_reader :piece
   
+  def initialize
+    @piece = "x"
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe Player  do
     it "can exist" do 
       expect(@player_1).to be_a Player
     end
+
+    it "has a piece to play that is x" do 
+      expect(@player_1.piece).to eq("x")
+    end
   end
 end
