7963ab4ce8c43d47cd116ee0cbb02c92f466ebaf
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-20 22:00:25 -0600

    feat: add start_game to game_controller (no test, user input)

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -1,3 +1,19 @@
+require_relative 'board'
+
 class GameController
-  
+  def start_game
+    board = Board.new
+    puts "Welcome to Connect 4!!!"
+    puts "Enter P to start a game, or Q to quit."
+    user_input = gets.chomp
+    while user_input != "p" && "q"
+      "Please enter P to start a game, or Q to quit."
+      user_input = gets.chomp
+    end
+    if user_input == "p"
+      board.render_board
+    else
+      exit()
+    end
+  end
 end
