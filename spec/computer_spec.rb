6782983c3e14659502b8e6365cf5b7648bbcafbe
Antoine Aube <aaube3@gmail.com>
2023-07-22 10:18:44 -0600

    feat: computer has a piece with corresponding test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 RSpec.describe Computer do 
   before do 
-    computer = Computer.new
+    @computer = Computer.new
   end
   describe "#initialize" do 
     it "exists" do 
-
+    
+      expect(@computer).to be_a Computer
     end
+
+    it "has a piece that is o" do 
+
+      expect(@computer.piece).to eq("o")
+    end 
   end 
 end
