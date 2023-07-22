90d79cc1bf6169cd83ba28139a986e618545ddae
Antoine Aube <aaube3@gmail.com>
2023-07-22 16:41:13 -0600

    feat: board_is_full? with corresponding test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -119,5 +119,29 @@ RSpec.describe Turn do
       expect(board.board_grid[4][0].state).to eq(".")
       expect(board.board_grid[5][0].state).to eq("x")
     end
-  end 
+  end
+  
+  describe "#board_is_full" do 
+    it "returns true if the first row is full" do 
+      player = Player.new
+      board = Board.new
+      turn = Turn.new(player, board)
+
+      
+      expect(turn.board_is_full?).to eq(false)
+      
+      board.board_grid[0][0].set_state("x")
+      board.board_grid[0][1].set_state("x")
+      board.board_grid[0][2].set_state("x")
+      board.board_grid[0][3].set_state("x")
+      board.board_grid[0][4].set_state("x")
+      board.board_grid[0][5].set_state("x")
+
+      expect(turn.board_is_full?).to eq(false)
+
+      board.board_grid[0][6].set_state("x")
+            
+      expect(turn.board_is_full?).to eq(true)
+    end
+  end
 end
