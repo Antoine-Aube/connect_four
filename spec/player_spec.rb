e6ac3008269fa53f34c4055e9a122f6286e7e16b
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-22 19:44:00 -0600

    feat: add winner to player with tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,5 +13,18 @@ RSpec.describe Player  do
     it "has a piece to play that is x" do 
       expect(@player_1.piece).to eq("x")
     end
+
+  end
+  
+  describe "#winner" do
+    it "can be marked as a winner" do
+      expect(@player_1.winner).to be false
+    end
+
+    it "can be set to true" do
+      @player_1.winner = true
+
+      expect(@player_1.winner).to be true
+    end
   end
 end
