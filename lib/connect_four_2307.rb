4dea1cd4b2411d0f7a7d912e86e069ff0b012f2c
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-26 14:23:21 -0600

    feat: initialize gem files

---
+++ lib/connect_four_2307.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "connect_four_2307/version"
+
+module ConnectFour2307
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/connect_four_2307_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ConnectFour2307 do
+  it "has a version number" do
+    expect(ConnectFour2307::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
